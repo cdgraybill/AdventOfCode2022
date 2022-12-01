622a83a0bfab3bc35a450f5e2026f7fd335faaf1
cdgraybill <cdgraybill@gmail.com>
2022-12-01 00:36:36 -0600

    day 01 solved

---
+++ AdventOfCode2022/Solvers/Day01Solver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Solvers
+{
+    public class Day01Solver
+    {
+        public readonly List<string> ProblemInput = File.ReadLines(@"C:ProblemInputs\Day01Input.txt").ToList();
+
+        public List<int> GetHaulsOfAllElves(List<string> sampleInput)
+        {
+            var hauls = new List<int>();
+            var totalCalories = 0;
+
+            foreach (string line in sampleInput)
+            {
+                if (line != "")
+                {
+                    var food = int.Parse(line);
+                    totalCalories += food;
+                }
+                else
+                {
+                    hauls.Add(totalCalories);
+                    totalCalories = 0;
+                }
+            }
+
+            return hauls;
+        }
+
+        public int GetLargestCalorieAmount(List<int> hauls)
+        {
+            return hauls.Max();
+        }
+
+        public int GetTotalOfTopThreeCalorieAmounts(List<int> hauls)
+        {
+            int first = -1;
+            int second = -1;
+            int third = -1;
+
+            foreach (int haul in hauls)
+            {
+                if (haul > first) 
+                {
+                    third = second;
+                    second = first; 
+                    first = haul; 
+                }
+                else if (haul > second) 
+                { 
+                    second = haul; 
+                }
+                else if (haul > third)
+                {
+                    third = haul;
+                }
+            }
+
+            return first + second + third;
+        }
+    }
+}
---
+++ AdventOfCode2022Tests/UnitTest1.cs
@@ -0,0 +1,60 @@
+using AdventOfCode2022.Solvers;
+
+namespace AdventOfCode2022Tests
+{
+    public class Tests
+    {
+        private List<string> sampleInput = new List<string>()
+        {
+            "1000",
+            "2000",
+            "3000",
+            "",
+            "4000",
+            "",
+            "5000",
+            "6000",
+            "",
+            "7000",
+            "8000",
+            "9000",
+            "",
+            "10000"
+        };
+
+        [Test]
+        public void GetHaulsOfAllElves()
+        {
+            var solver = new Day01Solver();
+
+            var hauls = solver.GetHaulsOfAllElves(sampleInput);
+
+            Assert.That(hauls, Does.Contain(6000));
+            Assert.That(hauls, Does.Contain(4000));
+            Assert.That(hauls, Does.Contain(11000));
+            Assert.That(hauls, Does.Contain(24000));
+        }
+
+        [Test]
+        public void GetTotalOfTopThreeCalorieAmounts_Solve()
+        {
+            var solver = new Day01Solver();
+
+            var hauls = solver.GetHaulsOfAllElves(solver.ProblemInput);
+            var totalCalories = solver.GetTotalOfTopThreeCalorieAmounts(hauls);
+
+            Assert.That(totalCalories, Is.EqualTo(209691));
+        }
+
+        [Test]
+        public void GetLargestCalorieAmount_Solve()
+        {
+            var solver = new Day01Solver();
+
+            var hauls = solver.GetHaulsOfAllElves(solver.ProblemInput);
+            var largestCalorieAmount = solver.GetLargestCalorieAmount(hauls);
+
+            Assert.That(largestCalorieAmount, Is.EqualTo(71300));
+        }
+    }
+}
