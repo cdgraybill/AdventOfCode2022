9cf77cf4ee72ef7e3c5a350723f5017387af0778
cdgraybill <cdgraybill@gmail.com>
2022-12-08 13:27:31 -0600

    day 6 done

---
+++ AdventOfCode2022/Solvers/Day06Solver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Solvers
+{
+    public class Day06Solver
+    {
+        public readonly List<string> Datastream = File.ReadAllLines(@"C:ProblemInputs\Day06Input.txt").ToList();
+
+        public int CountCharacters_PartOne(List<string> input)
+        {
+            var datastream = input[0];
+            var characterCount = 0;
+
+            for (int i = 1; i < datastream.Length; i++)
+            {
+                var buffer = datastream.Substring(i - 1, 4);
+                var hasUniqueCharacters = buffer.Distinct().Count() == buffer.Length;
+
+                if (hasUniqueCharacters)
+                {
+                    characterCount = i + 3;
+                    break;
+                }
+            }
+
+            return characterCount;
+        }
+
+        public int CountCharacters_PartTwo(List<string> input)
+        {
+            var datastream = input[0];
+            var characterCount = 0;
+
+            for (int i = 1; i < datastream.Length; i++)
+            {
+                var buffer = datastream.Substring(i - 1, 14);
+                var hasUniqueCharacters = buffer.Distinct().Count() == buffer.Length;
+
+                if (hasUniqueCharacters)
+                {
+                    characterCount = i + 13;
+                    break;
+                }
+            }
+
+            return characterCount;
+        }
+    }
+}
---
+++ AdventOfCode2022Tests/Day06Tests.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode2022.Solvers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022Tests
+{
+    public class Day06Tests
+    {
+        [Test]
+        public void PartOne_Solve()
+        {
+            var solver = new Day06Solver();
+            var characterCount = solver.CountCharacters_PartOne(solver.Datastream);
+
+            Assert.That(characterCount, Is.EqualTo(1929));
+        }
+
+        [Test]
+        public void PartTwo_Solve()
+        {
+            var solver = new Day06Solver();
+            var characterCount = solver.CountCharacters_PartTwo(solver.Datastream);
+
+            Assert.That(characterCount, Is.EqualTo(3298));
+        }
+    }
+}
